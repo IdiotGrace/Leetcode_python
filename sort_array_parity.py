d054cdbfb6f2e0ab6f1ef3b2541612bf57f7c90a
haowenchao <haowenchao123@gmail.com>
2019-09-02 16:19:48 +0800

    left modified

---
+++ sort_array_parity.py
@@ -0,0 +1,38 @@
+'''
+给定一个非负整数数组 A，返回一个由 A 的所有偶数元素组成的数组，后面跟 A 的所有奇数元素。
+
+你可以返回满足此条件的任何数组作为答案。
+
+ 
+
+示例：
+
+输入：[3,1,2,4]
+输出：[2,4,3,1]
+输出 [4,2,3,1]，[2,4,1,3] 和 [4,2,1,3] 也会被接受。
+ 
+
+提示：
+
+1 <= A.length <= 5000
+0 <= A[i] <= 5000
+'''
+
+class Solution:
+	def sortArrayByParity(self, A):
+		"""
+		:type A: List[int]
+		:rtype: List[int]
+		"""
+		i = 0
+		for j in range(len(A)):
+			if A[j] % 2 == 0:
+				A[i], A[j] = A[j], A[i]
+				i += 1
+
+		return A
+
+
+if __name__ == '__main__':
+	sol = Solution()
+	print(sol.sortArrayByParity([3, 1, 2, 4]))
